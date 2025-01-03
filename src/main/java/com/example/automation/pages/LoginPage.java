a3e9d58527da10df72278c9299c775029c597eff
Swati <115008404+swatimry@users.noreply.github.com>
2025-01-03 18:41:32 +0530

    Update LoginPage.java

--- src/main/java/com/example/automation/pages/LoginPage.java
+++ src/main/java/com/example/automation/pages/LoginPage.java
@@ -1,29 +1,51 @@
-package com.example.automation.pages;
+package com.example.automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage {
-    private String username;
-    private String password;
-    private boolean isLoggedIn;
+    private final WebDriver driver;
 
-    public LoginPage() {
-        this.isLoggedIn = false;
+   
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
     }
 
+   
+    public void openLoginPage(String url) {
+        driver.get(url);
+    }
+
+   
     public void setUsername(String username) {
-        this.username = username;
+        WebElement usernameField = driver.findElement(By.id("username"));
+        usernameField.sendKeys(username);
     }
 
+    
     public void setPassword(String password) {
-        this.password = password;
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys(password);
     }
 
-    public void clickLogin() {
-        if (username.equals("testuser") && password.equals("password123")) {
-            isLoggedIn = true;
-        }
+    
+    public void clickLoginButton() {
+        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        loginButton.click();
     }
 
-    public boolean isLoggedIn() {
-        return isLoggedIn;
+    
+    public String getSuccessMessage() {
+        WebElement messageElement = driver.findElement(By.id("flash"));
+        return messageElement.getText();
+    }
+
+    
+
+    
+    public String getErrorMessage() {
+        WebElement errorElement = driver.findElement(By.id("error"));
+        return errorElement.getText();
     }
 }
